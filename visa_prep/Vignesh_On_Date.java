0c6660bcf8f4ea48cec14a0e07f5cb63ee5a3d1c
MSonalika <sonalikamanchi321@gmail.com>
2024-11-15 18:15:19 +0530

    Create Vignesh_On_Date.java

---
+++ visa_prep/Vignesh_On_Date.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextLong();
+        long b=sc.nextLong();
+        if(a>=b){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
