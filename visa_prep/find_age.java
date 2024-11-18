859508ff89cd000791fa012aad9e9b620085c4f4
MSonalika <sonalikamanchi321@gmail.com>
2024-11-18 12:05:14 +0530

    Create find_age.java

---
+++ visa_prep/find_age.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        System.out.println(age-15);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}find_Age
