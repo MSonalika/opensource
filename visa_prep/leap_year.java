8eed0fb8020bbb635f307be447dcb5423b97a2f6
MSonalika <sonalikamanchi321@gmail.com>
2024-11-18 12:43:40 +0530

    Create leap_year.java

---
+++ visa_prep/leap_year.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%4==0&&n%100!=0||n%400==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+            
+        
+                
+        }
+    
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
